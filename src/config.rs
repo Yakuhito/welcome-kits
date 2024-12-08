e061104c35da15f8f48ffcd2b9f6be261c70f3e4
Yak <y@kuhi.to>
2024-12-08 09:27:53 -0300

    mainnet config

--- src/config.rs
+++ src/config.rs
@@ -10,22 +10,22 @@ pub const LOW_FUNDS_THRESHOLD: u64 = 4_200_000_000_000; // 0.42 XCH
 pub const WALLET_START_HEIGHT: u64 = 6_300_000;
 
 // for mainnet
-// pub const MAX_MOJOS_IN_ELIGIBLE_WALLETS: u64 = 10_000_000_000; // 0.01 XCH
-// pub const ELIGIBLE_SYMBOLS_AND_MINIMUM_AMOUNTS: [(&str, u64); 4] = [
-//     ("milliETH", 10_000),     // 10 milliETH, or 0.01 ETH
-//     ("USDC", 42_000),         // 40 USDC
-//     ("USDT", 42_000),         // 42 USDT
-//     ("XCH", 100_000_000_000), // 0.1 XCH
-// ];
+pub const MAX_MOJOS_IN_ELIGIBLE_WALLETS: u64 = 10_000_000_000; // 0.01 XCH
+pub const ELIGIBLE_SYMBOLS_AND_MINIMUM_AMOUNTS: [(&str, u64); 4] = [
+    ("milliETH", 10_000),     // 10 milliETH, or 0.01 ETH
+    ("USDC", 42_000),         // 40 USDC
+    ("USDT", 42_000),         // 42 USDT
+    ("XCH", 100_000_000_000), // 0.1 XCH
+];
 
 // for testing
-pub const MAX_MOJOS_IN_ELIGIBLE_WALLETS: u64 = 100_000_000_000_000; // 100 XCH
-pub const ELIGIBLE_SYMBOLS_AND_MINIMUM_AMOUNTS: [(&str, u64); 4] = [
-    ("milliETH", 1), // 0.001 milliETH
-    ("USDC", 1),     // 0.001 USDC
-    ("USDT", 1),     // 0.001 USDT
-    ("XCH", 1),      // 1 mojo
-];
+// pub const MAX_MOJOS_IN_ELIGIBLE_WALLETS: u64 = 100_000_000_000_000; // 100 XCH
+// pub const ELIGIBLE_SYMBOLS_AND_MINIMUM_AMOUNTS: [(&str, u64); 4] = [
+//     ("milliETH", 1), // 0.001 milliETH
+//     ("USDC", 1),     // 0.001 USDC
+//     ("USDT", 1),     // 0.001 USDT
+//     ("XCH", 1),      // 1 mojo
+// ];
 
 pub const PORTAL_LAUNCHER_ID: [u8; 32] =
     hex!("46e2bdbbcd1e372523ad4cd3c9cf4b372c389733c71bb23450f715ba5aa56d50");
